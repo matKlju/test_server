1a65ea39e925ce2d63317152ae8332f27ad50586
matKlju <108662553+matKlju@users.noreply.github.com>
2025-04-08 22:41:12 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+// server.js (repo1)
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+app.get('/', async (req, res) => {
+  const response = await axios.get('http://localhost:5000/number');
+  res.send(`<h1>Number: ${response.data}</h1>`);
+});
+
+app.listen(3000, () => console.log('App running on http://localhost:3000'));
