6a07e815b373fff270517a67ffb5d68201a0d7f9
Mati Kljukin <matik@matik-5420.ria.ee>
2025-04-19 23:13:01 +0300

    setup

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
-// server.js (test_server)
+// server.js
 const express = require('express');
 const axios = require('axios');
 const app = express();
 
 app.get('/', async (req, res) => {
-  const response = await axios.get('http://localhost:5000/number');
-  res.send(`<h1>Number: ${response.data}</h1>`);
+  try {
+    const response = await axios.get('http://localhost:5000/number');
+    res.send(`
+      <html>
+        <head><title>Number Viewer</title></head>
+        <body>
+          <h1>Number from repo2</h1>
+          <p data-testid="random-number">${response.data}</p>
+        </body>
+      </html>
+    `);
+  } catch (error) {
+    res.status(500).send('Failed to fetch number');
+  }
 });
 
 app.listen(3000, () => console.log('App running on http://localhost:3000'));
